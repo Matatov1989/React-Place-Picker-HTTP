d4ed4b2d5d37993c1367724fdb194aa3d9ddf98a
Yury <matatov1989@gmail.com>
2023-11-04 11:43:10 +0200

    create requesr

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,9 +3,13 @@ import { useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState(places);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  
+  fetch('http://localhost:3000/places').then((response) => {
+    return response.json()
+  }).then((resData) => {
+    setAvailablePlaces(resData.places);
+  });
 
   return (
     <Places
